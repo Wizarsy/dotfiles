2e377f4475c87d57bcf85806d55c5b897a11eb31
Wizarsy <filipemadruga@outlook.com>
2025-05-02 20:21:37 -0300

    backup: 02-05-2025 20:21:37

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,14 +1,14 @@
-local wezterm = require "wezterm"
+local wezterm = require("wezterm")
 local config = {}
 
-local default_font = "CaskaydiaCove Nerd Font Mono"
+local default_font = "CaskaydiaMono Nerd Font Mono"
 -- config.term = "wezterm"
 
-config.color_scheme_dirs = {wezterm.config_dir .. "/themes"}
+config.color_scheme_dirs = { wezterm.config_dir .. "/themes" }
 config.color_scheme = "tokyonight"
 config.use_fancy_tab_bar = true
 config.window_frame = {
-  font_size = 12.0,
+	font_size = 12.0,
 }
 
 config.default_cursor_style = "BlinkingUnderline"
@@ -25,52 +25,46 @@ config.font = wezterm.font(default_font)
 config.font_size = 12.0
 config.line_height = 1.2
 config.font_rules = {
-  {
-    intensity = "Normal",
-    italic = true,
-    font = wezterm.font(
-      default_font, {
-      style = "Oblique"
-      }
-    )
-  },
-  {
-    intensity = "Bold",
-    italic = true,
-    font = wezterm.font(
-      default_font, {
-      style = "Oblique"
-      }
-    )
-  },
-  {
-    intensity = "Half",
-    italic = true,
-    font = wezterm.font(
-      default_font, {
-      style = "Oblique",
-      foreground = "#414868"
-      }
-    )
-  },
-  {
-    intensity = "Half",
-    italic = false,
-    font = wezterm.font(
-      "CaskaydiaCove Nerd Font Mono", {
-        foreground = "#414868"
-      }
-    )
-  }
+	{
+		intensity = "Normal",
+		italic = true,
+		font = wezterm.font(default_font, {
+			style = "Oblique",
+		}),
+	},
+	{
+		intensity = "Bold",
+		italic = true,
+		font = wezterm.font(default_font, {
+			style = "Oblique",
+		}),
+	},
+	{
+		intensity = "Half",
+		italic = true,
+		font = wezterm.font(default_font, {
+			style = "Oblique",
+			foreground = "#414868",
+		}),
+	},
+	{
+		intensity = "Half",
+		italic = false,
+		font = wezterm.font("CaskaydiaCove Nerd Font Mono", {
+			foreground = "#414868",
+		}),
+	},
 }
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-  config.default_prog = {"pwsh.exe", "-NoProfileLoadTime", "-NoLogo"}
-  config.default_domain = "WSL:Arch"
-  config.window_decorations = "RESIZE|INTEGRATED_BUTTONS"
+	config.default_prog = { "pwsh.exe", "-NoProfileLoadTime", "-NoLogo" }
+	config.default_domain = "WSL:Arch"
+	config.mux_enable_ssh_agent = false
+	config.window_decorations = "RESIZE|INTEGRATED_BUTTONS"
 elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
-    config.default_prog = {"/usr/bin/zsh"}
-    config.window_decorations = "NONE"
+	config.default_prog = { "/usr/bin/zsh" }
+	config.window_decorations = "NONE"
 end
 
 return config
+
