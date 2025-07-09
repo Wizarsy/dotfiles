7314c54e86235bd85481c5000096a27fdc324564
Wizarsy <filipemadruga@outlook.com>
2025-07-08 21:56:22 -0300

    backup: 08-07-2025 21:56:22

--- .config/yadm/bootstrap.d/ssh-remote.sh
+++ .config/yadm/bootstrap.d/ssh-remote.sh
@@ -1,3 +1,6 @@
-#!/usr/bin/env sh
-echo 1
-# yadm remote set-url origin "git@github.com:Wizarsy/dotfiles.git"
+#!/usr/bin/env bash
+
+if [[ $(yadm remote get-url origin) =~ ^https ]]; then
+  echo "Updating the yadm repo origin URL"
+  yadm remote set-url origin "git@github.com:Wizarsy/dotfiles.git"
+fi
