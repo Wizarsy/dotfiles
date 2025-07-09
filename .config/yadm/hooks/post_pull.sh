d5d81211bf754e256e83a02db3a07490152e044c
Wizarsy <filipemadruga@outlook.com>
2025-07-09 00:42:23 -0300

    backup: 09-07-2025 00:42:23

---
+++ .config/yadm/hooks/post_pull.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "Running Post Pull Script..."
+
+if [[ "$(yadm config --get local.class)" == "wsl" ]]; then
+  if ! diff "$HOME/.config/wezterm/wezterm.lua" "/mnt/c/Users/Filipe/.config/wezterm/wezterm.lua" &> /dev/null; then
+    if [[ "$HOME/.config/wezterm/wezterm.lua" -nt "/mnt/c/Users/Filipe/.config/wezterm/wezterm.lua" ]]; then
+      cp -f "$HOME/.config/wezterm/wezterm.lua" "/mnt/c/Users/Filipe/.config/wezterm/wezterm.lua"
+    else
+      cp -f "/mnt/c/Users/Filipe/.config/wezterm/wezterm.lua" "$HOME/.config/wezterm/wezterm.lua"
+    fi
+  fi
+fi
