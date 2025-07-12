fd37685083053d4a8f96a7b6ab9f0576dfb4fc36
Wizarsy <filipemadruga@outlook.com>
2025-07-12 17:45:10 -0300

    backup: 12-07-2025 17:45:10

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,27 +6,27 @@ local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm"
 config.default_workspace = "master"
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	config.default_prog = { "pwsh.exe", "-NoProfileLoadTime", "-NoLogo" }
-	config.window_decorations = "RESIZE" -- "RESIZE|INTEGRATED_BUTTONS"
-	config.default_domain = "WSL:Arch"
-	for _, gpu in ipairs(gpus) do
-		if gpu.backend == "Dx12" and gpu.device_type == "DiscreteGpu" then
-			config.webgpu_preferred_adapter = gpu
-			config.front_end = "WebGpu"
-			break
-		end
-	end
+  config.default_prog = { "pwsh.exe", "-NoProfileLoadTime", "-NoLogo" }
+  config.window_decorations = "RESIZE" -- "RESIZE|INTEGRATED_BUTTONS"
+  config.default_domain = "WSL:Arch"
+  for _, gpu in ipairs(gpus) do
+    if gpu.backend == "Dx12" and gpu.device_type == "DiscreteGpu" then
+      config.webgpu_preferred_adapter = gpu
+      config.front_end = "WebGpu"
+      break
+    end
+  end
 elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
-	config.default_prog = { "/usr/bin/zsh" }
-	config.window_decorations = "NONE"
-	config.enable_wayland = true
-	-- for _, gpu in ipairs(gpus) do
-	--   if gpu.backend == "Vulkan" and gpu.device_type ~= "Cpu" then
-	--     config.webgpu_preferred_adapter = gpu
-	--     config.front_end = "WebGpu"
-	--     break
-	--   end
-	-- end
+  config.default_prog = { "/usr/bin/zsh" }
+  config.window_decorations = "NONE"
+  config.enable_wayland = true
+  for _, gpu in ipairs(gpus) do
+    if gpu.backend == "Vulkan" and gpu.device_type ~= "Cpu" then
+      config.webgpu_preferred_adapter = gpu
+      config.front_end = "WebGpu"
+      break
+    end
+  end
 end
 
 config.colors = require("colors.TokyoNight")
@@ -41,9 +41,22 @@ config.cursor_blink_rate = 500
 config.cursor_blink_ease_in = "Constant"
 config.cursor_blink_ease_out = "Constant"
 
-config.font_dirs = { "fonts" }
-config.font = wezterm.font_with_fallback({ "Monocraft", "Symbols Nerd Font Mono" })
-config.font_size = 12
+config.font_dirs = {"fonts"}
+config.font = wezterm.font_with_fallback(
+  {
+    {
+      family = "Monocraft",
+    },
+    {
+      family = "Symbols Nerd Font Mono",
+    },
+    {
+      family = "DepartureMono Nerd Font Mono"
+    }
+  }
+)
+config.freetype_load_flags = "FORCE_AUTOHINT"
+config.font_size = 13
 config.line_height = 1.2
 config.cell_width = 1.0
 
@@ -53,4 +66,3 @@ config.window_close_confirmation = "NeverPrompt"
 bar.apply_to_config(config)
 
 return config
-
