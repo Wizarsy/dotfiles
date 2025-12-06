7a6d16b3a3535f250d4f11d954c1c127c3ff8a66
Wizarsy <filipemadruga@outlook.com>
2025-12-06 20:08:14 -0300

    backup: 06-12-2025 20:08:14

--- .config/yadm/hooks/utils/symlink.sh
+++ .config/yadm/hooks/utils/symlink.sh
@@ -2,7 +2,7 @@
 
 hooks_symlink() {
   local SYMLINK_D="$YADM_HOOK_DATA/symlink.d"
-  local -a cpto cpfrom
+  local -a cpto cpfrom stats
   
   if [[ ! -e "$YADM_HOOK_DIR/symlink" ]]; then
     return 0
@@ -18,17 +18,20 @@ hooks_symlink() {
       if [[ ! -d "$SYMLINK_D/$symlink" ]]; then
         mkdir -p "$SYMLINK_D/$symlink"
       fi
-      cp -v -r --preserve=all --update=older --strip-trailing-slashes "$HOME/$symlink"/* "$SYMLINK_D/$symlink"
-      cp -v -r --preserve=all --update=older --strip-trailing-slashes "$SYMLINK_D/$symlink"/* "$HOME/$symlink"
-    fi
-
+    cp -v -r --preserve=all --update=older --strip-trailing-slashes "$HOME/$symlink"/* "$SYMLINK_D/$symlink"
+    cp -v -r --preserve=all --update=older --strip-trailing-slashes "$SYMLINK_D/$symlink"/* "$HOME/$symlink"
+    touch --no-create "$HOME/$symlink"/* 
     #   for file in "$HOME/$symlink"/*; do
-    #     if [[ -e "$SYMLINK_D/$file" && $(stat --print="%Y" "$SYMLINK_D/$file") -gt $(stat --print="%Y" "$HOME/$symlink/$file") ]]; then
-    #       cp 
+    #     stats=$((stat --printf="%Y\n" "$HOME/$symlink/$file" "$SYMLINK_D/$symlink/$file"))
+    #     if [[ "${stats[0]}" -gt "${stats[1]}" ]]; then
     #       cpfrom+=("$file")
     #     else
     #       cpto+=("$file")
     #     fi
     #   done
+    fi
   done < "$YADM_HOOK_DIR/symlink"
 }
+
+      # cp -v -r --preserve=all --update=older --strip-trailing-slashes "$HOME/$symlink"/* "$SYMLINK_D/$symlink"
+      # cp -v -r --preserve=all --update=older --strip-trailing-slashes "$SYMLINK_D/$symlink"/* "$HOME/$symlink"
