7e05df9dacd72e70411a6eff47de93dfe45f8b83
Wizarsy <filipemadruga@outlook.com>
2025-12-06 20:50:43 -0300

    backup: 06-12-2025 20:50:43

--- .config/yadm/hooks/utils/symlink.sh
+++ .config/yadm/hooks/utils/symlink.sh
@@ -20,7 +20,9 @@ hooks_symlink() {
       fi
     cp -v -r --preserve=all --update=older --strip-trailing-slashes "$HOME/$symlink"/* "$SYMLINK_D/$symlink"
     cp -v -r --preserve=all --update=older --strip-trailing-slashes "$SYMLINK_D/$symlink"/* "$HOME/$symlink"
-    touch -c -d "$(date +'%F %T %z')" "$HOME/$symlink"/* "$SYMLINK_D/$symlink"/*
+    if [[ "$(</proc/version)" =~ [Mm]icrosoft ]]; then
+      touch -c -d "$(date +'%F %T %z')" "$HOME/$symlink"/* "$SYMLINK_D/$symlink"/*
+    fi
     #   for file in "$HOME/$symlink"/*; do
     #     stats=$((stat --printf="%Y\n" "$HOME/$symlink/$file" "$SYMLINK_D/$symlink/$file"))
     #     if [[ "${stats[0]}" -gt "${stats[1]}" ]]; then
