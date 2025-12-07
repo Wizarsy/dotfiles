05dd8b1d8b58098f920ccc60be002630902504c2
Wizarsy <filipemadruga@outlook.com>
2025-12-06 21:29:09 -0300

    backup: 06-12-2025 21:29:08

--- .config/yadm/hooks/utils/symlink.sh
+++ .config/yadm/hooks/utils/symlink.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 hooks_symlink() {
+  shopt -s nullglob
   local SYMLINK_D="$YADM_HOOK_DATA/symlink.d"
-  local -a cpto cpfrom stats
+  local -a home_d sym_d
   
   if [[ ! -e "$YADM_HOOK_DIR/symlink" ]]; then
     return 0
@@ -18,12 +19,15 @@ hooks_symlink() {
       if [[ ! -d "$SYMLINK_D/$symlink" ]]; then
         mkdir -p "$SYMLINK_D/$symlink"
       fi
-    cp -v -r --preserve=all --update=older --strip-trailing-slashes "$HOME/$symlink"/* "$SYMLINK_D/$symlink"
-    cp -v -r --preserve=all --update=older --strip-trailing-slashes "$SYMLINK_D/$symlink"/* "$HOME/$symlink"
-    if [[ "$(</proc/version)" =~ [Mm]icrosoft ]]; then
-      echo "wsl"
-      touch -c -m -d "$(date +'%F %T %z')" "$HOME/$symlink"/* "$SYMLINK_D/$symlink"/*
-    fi
+
+      home_d=("$HOME/$symlink"/*)
+      sym_d=("$SYMLINK_D/$symlink"/*)
+
+      cp -v -r --preserve=all --update=older --strip-trailing-slashes "${home_d[@]}" "$SYMLINK_D/$symlink"
+      cp -v -r --preserve=all --update=older --strip-trailing-slashes "${sym_d[@]}" "$HOME/$symlink"
+      if [[ "$(</proc/version)" =~ [Mm]icrosoft ]]; then
+        touch -c -m -d "$(date +'%F %T %z')" "${home_d[@]}" "${sym_d[@]}"
+      fi
     #   for file in "$HOME/$symlink"/*; do
     #     stats=$((stat --printf="%Y\n" "$HOME/$symlink/$file" "$SYMLINK_D/$symlink/$file"))
     #     if [[ "${stats[0]}" -gt "${stats[1]}" ]]; then
@@ -33,8 +37,11 @@ hooks_symlink() {
     #     fi
     #   done
     fi
+    # cp -v -r --preserve=all --update=older --strip-trailing-slashes "$HOME/$symlink" "$SYMLINK_D/$symlink"
+    # cp -v -r --preserve=all --update=older --strip-trailing-slashes "$SYMLINK_D/$symlink" "$HOME/$symlink"
+    # if [[ "$(</proc/version)" =~ [Mm]icrosoft ]]; then
+    #   touch -c -m -d "$(date +'%F %T %z')" "$HOME/$symlink" "$SYMLINK_D/$symlink"
+    # fi
   done < "$YADM_HOOK_DIR/symlink"
+  shopt -u nullglob
 }
-
-      # cp -v -r --preserve=all --update=older --strip-trailing-slashes "$HOME/$symlink"/* "$SYMLINK_D/$symlink"
-      # cp -v -r --preserve=all --update=older --strip-trailing-slashes "$SYMLINK_D/$symlink"/* "$HOME/$symlink"
