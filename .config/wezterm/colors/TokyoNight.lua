fb2e4f80128b2b1d1acf473a72928800e7c4fffe
Wizarsy <filipemadruga@outlook.com>
2025-12-11 10:33:32 -0300

    backup: 11-12-2025 10:33:32

---
+++ .config/wezterm/colors/TokyoNight.lua
@@ -0,0 +1,54 @@
+return {
+  foreground = "#a9b1d6",
+  background = "#1a1b26",
+  cursor_bg = "#9aa5ce",
+  cursor_border = "#1a1b26",
+  cursor_fg = "#565f89",
+  selection_bg = "#565f89",
+  selection_fg = "#9aa5ce",
+  ansi = {
+    "#414868",
+    "#f7768e",
+    "#9ece6a",
+    "#e0af68",
+    "#7aa2f7",
+    "#bb9af7",
+    "#7dcfff",
+    "#c0caf5",
+  },
+  brights = {
+    "#414868",
+    "#f7768e",
+    "#9ece6a",
+    "#e0af68",
+    "#7aa2f7",
+    "#bb9af7",
+    "#7dcfff",
+    "#c0caf5",
+  },
+  tab_bar = {
+    background = "transparent",
+    active_tab = {
+      bg_color = "#7aa2f7",
+      fg_color = "#1a1b26"
+    },
+    inactive_tab = {
+      bg_color = "#272a3d",
+      fg_color = "#414868"
+    },
+    inactive_tab_hover = {
+      bg_color = "#272a3d",
+      fg_color = "#414868",
+      intensity = "Bold",
+    },
+    new_tab = {
+      bg_color = "transparent",
+      fg_color = "#9ece6a"
+    },
+    new_tab_hover = {
+      bg_color = "#9ece6a",
+      fg_color = "transparent",
+      intensity = "Bold"
+    }
+  }
+}
