5093380de394de43b67a33eb7cba81578d20e488
Wizarsy <filipemadruga@outlook.com>
2026-01-15 17:46:53 -0300

    backup: 15-01-2026 17:46:52

--- .config/yadm/bootstrap.d/02-linux.sh
+++ .config/yadm/bootstrap.d/02-linux.sh
@@ -1,6 +1,36 @@
 #!/usr/bin/env bash
 
-{% include "include/{{ yadm.distro }}" %}
+INSTALLC="yay -S --noconfirm"
+SYNCC="yay -Sy"
+UPC="yay -Syu --noconfirm"
+CLEANC="yay -Yc --noconfirm"
+pkgs=(zsh jq fastfetch bat btop fd eza zoxide fzf ripgrep-all graphicsmagick pandoc poppler tesseract dust asdf-vm pkgfile openssh cronie ffmpeg imagemagick nvim wezterm)
+
+init() {
+  sudo -v || exit 1
+  sudo -v pacman -Sy
+  sudo -v pacman -S --noconfirm curl git man-db base-devel ca-certificates
+  git clone https://aur.archlinux.org/yay.git /tmp/yay
+  makepkg -si -D /tmp/yay
+  yay -Y --gendb
+  yay -Y --devel --save
+
+  "$UPC"
+  "$CLEANC"
+}
+
+yazi() {
+  "$INSTALLC" 7zip chafa resvg yazi
+  ya pkg add yazi-rs/plugins:chmod
+  ya pkg add yazi-rs/plugins:git
+  ya pkg add yazi-rs/plugins:mount
+  ya pkg add AnirudhG07/plugins-yazi:copy-file-contents
+}
+
+docker() {
+  "$INSTALLC" docker docker-buildx docker-compose lazydocker
+  sudo -v systemctl enable docker
+}
 
 essential() {
   "$INSTALLC" "${pkgs[@]}"
