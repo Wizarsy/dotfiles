5e02deb82ebd23514fd0137e9c7d0970ea0a33fa
Wizarsy <filipemadruga@outlook.com>
2026-04-20 00:09:59 -0300

    backup: 20-04-2026 00:09:59

--- .config/yadm/bootstrap.d/00-yadm.sh
+++ .config/yadm/bootstrap.d/00-yadm.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+declare -a _CLASSES
+
+_CLASSES=("minimal" "server" "hyprland")
+
 echo "Updating the yadm repo origin URL"
 yadm remote set-url origin "git@github.com:Wizarsy/dotfiles.git"
 
 echo "Configuring git"
 yadm gitconfig pull.rebase false
+
+echo "Choose a profile"
+while read -ren 1 opt; do
+  [[ "$opt" -eq 0 ]] && break
+done
