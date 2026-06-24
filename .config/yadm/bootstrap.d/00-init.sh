93f88e6c7824a5df65020afe0ee59c1ebe3ed61b
Wizarsy <filipemadruga@outlook.com>
2026-06-23 21:18:37 -0300

    backup: 23-06-2026 21:18:36

--- .config/yadm/bootstrap.d/00-init.sh
+++ .config/yadm/bootstrap.d/00-init.sh
@@ -10,7 +10,7 @@ case $_HOST in
     YADM_UPDATE_REMOTE=1
     ;;
   VOSTRO-5320)
-    YADM_CLASSES=(wayland hyprland openrc pacman btrfs ssd zram dev)
+    YADM_CLASSES=(wayland hyprland openrc pacman btrfs ssd zram dev mobile)
     YADM_UPDATE_REMOTE=1
     ;;
   *)
@@ -28,16 +28,3 @@ yadm config --add local.class minimal
 for class in "${YADM_CLASSES[@]}"; do
   yadm config --add local.class "${class}"
 done
-
-# while read -ren 1 opt; do
-#   case $opt in
-#     0) yadm config --add local.class server;;
-#     1) yadm config --add local.class dev;;
-#     2)
-#       yadm config --add local.class wayland
-#       yadm config --add local.class hyprland
-#       yadm config --add local.class DE;;
-#     q) break;;
-#     *) continue;;
-#   esac
-# done
