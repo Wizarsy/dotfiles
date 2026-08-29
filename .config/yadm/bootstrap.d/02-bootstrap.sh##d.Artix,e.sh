8312c7ec1fc867b4b135447a6aa0ef6a36eb4106
Wizarsy <filipemadruga@outlook.com>
2026-08-29 01:48:50 -0300

    backup: 29-08-2026 01:48:50

--- .config/yadm/bootstrap.d/02-bootstrap.sh##d.Artix,e.sh
+++ .config/yadm/bootstrap.d/02-bootstrap.sh##d.Artix,e.sh
@@ -219,8 +219,6 @@ openrc-bootstrap() {
   git clone https://github.com/Wizarsy/PKGBUILDs /tmp/PKGBUILDs
   makepkg --noconfirm --needed --asdeps -sri -D /tmp/PKGBUILDs/ananicy-cpp-git
   makepkg --noconfirm --needed --asdeps -sri -D /tmp/PKGBUILDs/bpftune-git
-  # yay -U --noconfirm --asdeps /tmp/PKGBUILDs/ananicy-cpp-git/*.zst
-  # yay -U --noconfirm --asdeps /tmp/PKGBUILDs/bpftune-git/*.zst
 
   local pkgs=(ananicy-cpp-openrc fwupd-openrc openssh-openrc power-profiles-daemon-openrc ufw-openrc scx_loader-openrc dbus-openrc ssh-agent-openrc)
   local sys_services=(fwupd power-profiles-daemon ufw scx_loader dbus ananicy-cpp)
@@ -260,10 +258,10 @@ openrc-bootstrap() {
   $INSTALL "${pkgs[@]}"
   sudo -n rc-update --update
   for srvc in "${sys_services[@]}"; do
-    sudo -n rc-update add "${srvc%-*}" default
+    sudo -n rc-update add "$srvc" default
   done
   for srvc in "${user_services[@]}"; do
-    rc-update -U add "${srvc%-*}" default
+    rc-update -U add "$srvc" default
   done
 }
 
